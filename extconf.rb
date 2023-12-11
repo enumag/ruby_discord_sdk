a168aa5703bb288e3d6ea2b0e8545f54c86bd8e5
Riley Pierce <rileyraine01@gmail.com>
2023-12-11 23:53:55 +0000

    better cross platform discord finding

--- extconf.rb
+++ extconf.rb
@@ -1,5 +1,10 @@
 require 'mkmf'
 
-p find_library('discord_game_sdk', nil)
+if /mswin|mingw/ =~ RUBY_PLATFORM
+    find_library("discord_game_sdk", nil)
+else
+    LIBRARY_DIR = "#{Dir.pwd}/lib/x86_64"
+    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
+ end
 
 create_makefile 'discord'
