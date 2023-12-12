c98fbecd4eec26e57d5dbbac4356df95c9470fde
Jáchym Toušek <enumag@gmail.com>
2023-12-12 16:07:32 +0100

    improve extconf

--- extconf.rb
+++ extconf.rb
@@ -2,9 +2,12 @@ require 'mkmf'
 
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
-else
-    LIBRARY_DIR = "#{Dir.pwd}/lib/x86_64"
-    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
- end
+elsif /linux/ =~ RUBY_PLATFORM
+    $LDFLAGS += " -L./lib/x86_64 -Wl,-R. -l:discord_game_sdk.so"
+elsif /macos/ =~ RUBY_PLATFORM
+    $LDFLAGS += " -L./lib/x86_64 -L./lib/aarch64 -Wl,-R. -arch x86_64 -arch arm64 -l:discord_game_sdk.dylib"
+end
+
+puts $LDFLAGS
 
 create_makefile 'discord'
