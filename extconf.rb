3026dfcab792ddd0b92a47ae2a1320e9ec414db3
Jáchym Toušek <enumag@gmail.com>
2023-12-12 20:28:15 +0100

    dylib

--- extconf.rb
+++ extconf.rb
@@ -3,9 +3,9 @@ require 'mkmf'
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /linux/ =~ RUBY_PLATFORM
-    $LDFLAGS += " -L./lib/x86_64 -Wl,--rpath=. -l:discord_game_sdk.so"
+    $LDFLAGS += " -L./lib/x86_64 -Wl,-R. -l:discord_game_sdk.so"
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += " -v -L. -l:discord_game_sdk.dylib"
+    $LDFLAGS += " -L./lib/x86_64 -rpath . -ldiscord_game_sdk"
 else
     raise "Unknown RUBY_PLATFORM: " + RUBY_PLATFORM
 end
