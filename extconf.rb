f9bd0f7366c0057264852b4a7e00f031c48ae877
Riley Pierce <rileyraine01@gmail.com>
2023-12-12 19:15:36 +0000

    Update extconf.rb

--- extconf.rb
+++ extconf.rb
@@ -5,9 +5,9 @@ LIBRARY_DIR = "#{Dir.pwd}/lib/x86_64"
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -rpath. -rpath./lib -l:discord_game_sdk.dylib'
+    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -rpath . -rpath ./lib -l:discord_game_sdk.dylib'
 else
-    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-rpath. -Wl,-rpath./lib -l:discord_game_sdk.so'
+    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
 end
 
 create_makefile 'discord'
