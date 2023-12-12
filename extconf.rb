7cbb5cc039649cea404dc979c28b06b90cdf6666
Jáchym Toušek <enumag@gmail.com>
2023-12-12 16:33:57 +0100

    Add workflow

--- extconf.rb
+++ extconf.rb
@@ -5,7 +5,7 @@ LIBRARY_DIR = "#{Dir.pwd}/lib/x86_64"
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk'
+    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch arm64'
 else
     $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
 end
