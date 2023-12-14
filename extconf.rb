d942711e2a69d04614f8307b2d9f7bb646bd13c7
Riley Pierce <rileyraine01@gmail.com>
2023-12-14 15:03:17 +0000

    windows changes

--- extconf.rb
+++ extconf.rb
@@ -3,7 +3,7 @@ require 'mkmf'
 LIBRARY_DIR = "#{Dir.pwd}/lib/#{ARGV[0]}"
 
 if /mswin|mingw/ =~ RUBY_PLATFORM
-    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.lib'
+    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -ldiscord_game_sdk'
 elsif /darwin/ =~ RUBY_PLATFORM
     $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch ' + ARGV[0] + ' '
     $ARCH_FLAG += '-arch ' + ARGV[0]
