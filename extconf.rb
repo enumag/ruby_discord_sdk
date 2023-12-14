09cae0a0f497524e18d1c5b0fa7060e6809ff501
Jáchym Toušek <enumag@gmail.com>
2023-12-14 17:05:01 +0100

    ...

--- extconf.rb
+++ extconf.rb
@@ -1,16 +1,14 @@
 require 'mkmf'
 
-LIBRARY_DIR = "#{Dir.pwd}/lib/x86_64"
-
 puts 'before'
 puts $LDFLAGS
 
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += ' -v -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch arm64'
+    $LDFLAGS += ' -v -L. -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch arm64'
 else
-    $LDFLAGS += ' -v -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
+    $LDFLAGS += ' -v -L. -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
 end
 
 puts 'after'
