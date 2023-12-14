aba0460a57a799cb1a41124953ffbec16634f77a
Riley Pierce <rileyraine01@gmail.com>
2023-12-14 13:57:55 +0000

    edit extconf.rb

--- extconf.rb
+++ extconf.rb
@@ -6,8 +6,8 @@ LIBRARY_DIR_ARM = "#{Dir.pwd}/lib/aarch64"
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += '-arch x86_64 -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk'
-    $LDFLAGS += '-arch arm64 -L' + LIBRARY_DIR_ARM + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk'
+    $LDFLAGS += '-L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch x86_64'
+    $LDFLAGS += '-L' + LIBRARY_DIR_ARM + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch arm64'
 else
     $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
 end
