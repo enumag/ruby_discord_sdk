9e4178f5dd9affdd8992b12e2af752d0124d6566
Jáchym Toušek <enumag@gmail.com>
2023-12-14 17:09:11 +0100

    ...

--- extconf.rb
+++ extconf.rb
@@ -6,7 +6,7 @@ puts $LDFLAGS
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += ' -v -L. -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch arm64'
+    $LDFLAGS += ' -v -L. -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk'
 else
     $LDFLAGS += ' -v -L. -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
 end
