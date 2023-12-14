65ca15910ec3c46df82a87e7003e5f06492399f0
Riley Pierce <rileyraine01@gmail.com>
2023-12-14 14:16:01 +0000

    further macos cross compilation experiments

--- extconf.rb
+++ extconf.rb
@@ -1,13 +1,12 @@
 require 'mkmf'
 
-LIBRARY_DIR = "#{Dir.pwd}/lib/x86_64"
-LIBRARY_DIR_ARM = "#{Dir.pwd}/lib/aarch64"
+LIBRARY_DIR = "#{Dir.pwd}/lib/#{ARGV[0]}"
 
 if /mswin|mingw/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil)
 elsif /darwin/ =~ RUBY_PLATFORM
-    $LDFLAGS += '-L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch x86_64 '
-    $LDFLAGS += '-L' + LIBRARY_DIR_ARM + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch arm64 '
+    $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch ' + ARGV[0] + ' '
+    $ARCH_FLAG += '-arch ' + ARGV[0]
 else
     $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
 end
