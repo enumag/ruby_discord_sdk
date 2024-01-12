7e43ccb73fd376c145d521abf67684c03285b5db
Riley Pierce <rileyraine01@gmail.com>
2024-01-12 21:05:07 +0000

    connect disconnect

--- test.rb
+++ test.rb
@@ -2,3 +2,19 @@ require './discord'
 puts "Successfully loaded discord extension"
 Discord.connect(551158360083398656)
 puts "Connected to Discord!" if Discord.connected?
+Discord.update_activity({details:"test"})
+Discord.update
+Discord.update
+Discord.update
+gets
+Discord.update_activity({})
+Discord.update
+Discord.update
+Discord.update
+gets
+Discord.disconnect
+gets 
+Discord.connect(551158360083398656)
+puts "Reconnected to Discord!" if Discord.connected?
+gets
+Discord.disconnect
