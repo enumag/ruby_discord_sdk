9b104a7c4182da37bf554885baf6c13f81efad87
Jáchym Toušek <enumag@gmail.com>
2024-10-26 11:22:28 +0200

    suppress error

--- extconf.rb
+++ extconf.rb
@@ -11,6 +11,7 @@ if /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
     find_library("discord_game_sdk", nil, LIBRARY_DIR)
 elsif /darwin/ =~ RUBY_PLATFORM
     $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-rpath,. -Wl,-rpath,./lib -ldiscord_game_sdk -arch ' + ARGV[0] + ' '
+    $CPPFLAGS += '-Wno-incompatible-function-pointer-types'
     $ARCH_FLAG += '-arch ' + ARGV[0]
 else
     $LDFLAGS += ' -L' + LIBRARY_DIR + ' -Wl,-R. -Wl,-R./lib -l:discord_game_sdk.so'
